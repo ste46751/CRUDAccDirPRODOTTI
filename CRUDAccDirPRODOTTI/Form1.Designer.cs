04b882a7bbb587601702f5503914ac5c289846c1
ste46751 <stefanoquarti060@gmail.com>
2023-11-02 19:10:50 +0100

    Aggiungere i file di progetto.

---
+++ CRUDAccDirPRODOTTI/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CRUDAccDirPRODOTTI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_Prezzo = new System.Windows.Forms.TextBox();
+            this.bttn_aggiungi = new System.Windows.Forms.Button();
+            this.bttn_file = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(58, 97);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(127, 22);
+            this.txt_Nome.TabIndex = 0;
+            // 
+            // txt_Prezzo
+            // 
+            this.txt_Prezzo.Location = new System.Drawing.Point(261, 97);
+            this.txt_Prezzo.Name = "txt_Prezzo";
+            this.txt_Prezzo.Size = new System.Drawing.Size(127, 22);
+            this.txt_Prezzo.TabIndex = 1;
+            // 
+            // bttn_aggiungi
+            // 
+            this.bttn_aggiungi.Location = new System.Drawing.Point(557, 60);
+            this.bttn_aggiungi.Name = "bttn_aggiungi";
+            this.bttn_aggiungi.Size = new System.Drawing.Size(180, 59);
+            this.bttn_aggiungi.TabIndex = 2;
+            this.bttn_aggiungi.Text = "Aggiungi Prodotto";
+            this.bttn_aggiungi.UseVisualStyleBackColor = true;
+            this.bttn_aggiungi.Click += new System.EventHandler(this.bttn_aggiungi_Click);
+            // 
+            // bttn_file
+            // 
+            this.bttn_file.Location = new System.Drawing.Point(58, 12);
+            this.bttn_file.Name = "bttn_file";
+            this.bttn_file.Size = new System.Drawing.Size(180, 59);
+            this.bttn_file.TabIndex = 3;
+            this.bttn_file.Text = "Crea File";
+            this.bttn_file.UseVisualStyleBackColor = true;
+            this.bttn_file.Click += new System.EventHandler(this.bttn_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 534);
+            this.Controls.Add(this.bttn_file);
+            this.Controls.Add(this.bttn_aggiungi);
+            this.Controls.Add(this.txt_Prezzo);
+            this.Controls.Add(this.txt_Nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_Prezzo;
+        private System.Windows.Forms.Button bttn_aggiungi;
+        private System.Windows.Forms.Button bttn_file;
+    }
+}
+
---
+++ CRUDAccDirPRODOTTI/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDAccDirPRODOTTI
+{
+    public partial class Form1 : Form
+    {
+        public struct Prodotto
+        {
+            public string Nome;
+            public float Prezzo;
+        }
+        public Prodotto[] p;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            p = new Prodotto[100];
+
+        }
+
+        private void bttn_file_Click(object sender, EventArgs e)
+        {
+            
+            FileStream f_in_out=new FileStream(@"./Lista.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter f_out = new BinaryWriter(f_in_out);
+            BinaryReader f_in = new BinaryReader(f_in_out);
+
+            string Nome = "@", Prezzo = "@",riga;
+            byte[] strInByte;
+            int TOTRecord = 100;
+
+            // preparo il file con 100 record vuoti
+            riga = Nome.PadRight(32) + Prezzo.PadRight(32);
+            strInByte = Encoding.Default.GetBytes(riga);
+            for (int i = 1; i <= TOTRecord; i++)
+            {
+                f_out.Write(strInByte);
+            }
+             
+            f_out.Close();
+            f_in_out.Close();
+
+        }
+
+        private void bttn_aggiungi_Click(object sender, EventArgs e)
+        {
+            FileStream f_in_out = new FileStream(@"./Lista.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter f_out = new BinaryWriter(f_in_out);
+            BinaryReader f_in = new BinaryReader(f_in_out);
+
+            int NumeroRecord = 1;
+            byte[] strInByte;
+            int size = 64;
+
+            string Nome = txt_Nome.Text, Prezzo = txt_Prezzo.Text;
+
+            string riga = Nome.PadRight(32) + Prezzo.PadRight(32);
+
+            strInByte = Encoding.Default.GetBytes(riga);  // 64 byte totali
+
+            // posizionamento IN SCRITTURA
+            f_out.BaseStream.Seek((NumeroRecord - 1) * size, SeekOrigin.Begin);
+
+            f_out.Write(strInByte);
+
+            f_out.Close();
+            f_in_out.Close();
+        }
+    }
+}
